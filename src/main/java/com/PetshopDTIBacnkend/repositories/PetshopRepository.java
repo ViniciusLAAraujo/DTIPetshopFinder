9b30870710592d930cf83f88482940cbf42b759a
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2023-08-23 16:10:37 -0300

    PetshopRepository added

---
+++ src/main/java/com/PetshopDTIBacnkend/repositories/PetshopRepository.java
@@ -0,0 +1,9 @@
+package com.PetshopDTIBacnkend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.PetshopDTIBacnkend.entities.Petshop;
+
+public interface PetshopRepository extends JpaRepository<Petshop, Long>{
+
+}
