b94b16e3c3998ea7bf6223f1832fbfdcc82f807c
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2023-08-23 15:59:43 -0300

    initial configurations

---
+++ src/main/java/com/PetshopDTIBacnkend/PetshopDtiBacnkendApplication.java
@@ -0,0 +1,13 @@
+package com.PetshopDTIBacnkend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopDtiBacnkendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopDtiBacnkendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PetshopDTIBacnkend/PetshopDtiBacnkendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PetshopDTIBacnkend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopDtiBacnkendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
